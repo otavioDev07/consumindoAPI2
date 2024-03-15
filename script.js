b2b85cd26eb87a0da196d8188c4f32602f783890
Otávio Neto <netootavio223@outlook.com>
2024-03-15 08:23:26 -0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 async function mostrarProdutos(){
     const apiURL ="https://7c50d5bb-c1cf-4c6e-9074-712d61828b26-00-3ulv4zods9g96.kirk.replit.dev/getall";
-    let dados = [];
     try{
         const consulta = await fetch(apiURL);
         console.log(consulta);
